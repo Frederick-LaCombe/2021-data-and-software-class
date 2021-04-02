cf8c9fcf85abf2076daa659d3b498bdc28361915
Frederick LaCombe <flacombe2@yahoo.com>
2021-04-02 14:16:40 -0400

    Setting up tests

--- tests/test_asserts.py
+++ tests/test_asserts.py
@@ -1,11 +1,12 @@
 import pytest
 import sys, os
+import numpy as np
 
 sys.path.append(os.path.join(
     os.path.dirname(__file__),
     "../"))
 
-from src import plotting
+from src.plotting as plotting
 
 plotting.plot()
 
