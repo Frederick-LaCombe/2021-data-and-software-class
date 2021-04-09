9d7d9e99b3de21e03c570ad71ca05a2a6e1fbc07
Frederick LaCombe <flacombe2@yahoo.com>
2021-04-09 14:36:22 -0400

    cleanup pylint messages

--- src/plotting.py
+++ src/plotting.py
@@ -1,16 +1,19 @@
 #!/bin/python
-
+"""This is a simple text editor"""
 # Import the libraries we are using. It is good practice to import all necessary
 # libraries in the first lines of a file.
+import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
-import sys
+
+
+
 
 # Create a function to read the data file
 def read_data(filename,delimiter=',',starting_row=0):
-    """This function reads data from a specified filename. 
+    """This function reads data from a specified filename.
     The specified filename should point to a .csv file."""
     # Create an array (a multi-dimensional table) out of our data file, full of text
     all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=5)
@@ -20,6 +23,8 @@ def read_data(filename,delimiter=',',starting_row=0):
     return temperature_data
 
 def process_data(temperature_data):
+    """Given some input temperature data this function converts the second column
+    from degrees F to K and appends a new column with that data. """
     # Compute a new column by multiplying column number 1 to Kelvin
     temperature_kelvin = (temperature_data[:,1,None] - 32) * 5/9 + 273
 
@@ -28,20 +33,29 @@ def process_data(temperature_data):
     return processed_temperature_data
 
 def plot_data(processed_temperature_data, plot_filename):
+    """ Given some input temperature data this function converts the second column
+    from degrees F to K and appends a new column with that data. """
     # Create a figure of the processed data
     temperature_figure = plt.figure()
-    temperature_plot = plt.bar (processed_temperature_data[:,0],processed_temperature_data[:,2], width=50, color='blue')
+    plt.bar (processed_temperature_data[:,0],
+                                processed_temperature_data[:,2],
+                                 width=50, color='blue')
 
     plt.show(block=True)
     temperature_figure.savefig(plot_filename)
 
 
 def convert_data(filename, output_filename):
+    """ Read data from a CSV falled filename, and write this
+     data into a Pandas dataframe, and write this dataframe
+     into a json file called output_filename. """
     all_data = pd.read_csv(filename, index_col='Date', header=4)
     all_data.info()
     all_data.to_json(output_filename)
 
 def plot():
+    """Maine program that reads a dataset and processes it, plots it, and write the
+     converted data into a json file"""
     input_file = "110-tavg-12-12-1950-2020.csv"
     plot_file = "temperature-over-time.pdf"
     json_output_file = "data_output.json"
